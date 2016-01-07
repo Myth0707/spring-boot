7d4318b1be8ec4dce66e5e9f37eaaade39e202ad
liliang10 <liliang10@CNBJWK5525TXB.letv.local>
2016-01-07 18:24:13 +0800

    @Controller change to @RestController
    Signed-off-by:

--- src/main/java/myth/com/spring_boot/HelloController.java
+++ src/main/java/myth/com/spring_boot/HelloController.java
@@ -2,17 +2,17 @@ package myth.com.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration 
-
+@RequestMapping("/hello") 
 public class HelloController {
   
-    @ResponseBody 
-    @RequestMapping("/hello") 
+	@RequestMapping
     public String hello(){   
         return "hello，Spring Boot ！";   
     }   
